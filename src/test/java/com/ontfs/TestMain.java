4bbb2df441b3d5cf349e45ba6a3fa3e9dc776c04
lijx <lijiaxin@onchain.com>
2020-09-28 10:25:05 +0800

    add assembly plugin

---
+++ src/test/java/com/ontfs/TestMain.java
@@ -0,0 +1,17 @@
+package com.ontfs;
+
+import org.testng.TestNG;
+import org.testng.collections.Lists;
+
+import java.util.List;
+
+public class TestMain {
+    public static void main(String[] args) {
+        TestNG tng = new TestNG();
+        List<String> suites = Lists.newArrayList();
+        //添加要执行的testng.xml文件
+        suites.add("testng.xml");
+        tng.setTestSuites(suites);
+        tng.run();
+    }
+}
